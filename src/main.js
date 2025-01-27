a9253c1112abf6687ca711032555a23c3d5fffe2
Eric <ericlien749@gmail.com>
2025-01-26 17:20:46 -0800

    Added info comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/*
+Eric Lien
+Rocket Patrol: Remastered!!!!!! (kinda)
+Time to complete: like 3 hours lol
+Mods: 
+    Mouse Control (5)
+    Particle Explosions (5)
+    Dynamic Timing/Scoring Mechanism (5)
+    Display Time Remaining (3)
+    Control Rocket After it's Fired (1)
+    Randomized Spaceship Speed (1)
+Sources:
+    Phaser Documentation
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
